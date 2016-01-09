4f6f28a4dfb2cb5f3a5a6b3c3cbf97aa2a11ff99
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2016-01-09 16:04:37 +0100

    maven project template

---
+++ src/main/java/com/pbw/app/App.java
@@ -0,0 +1,13 @@
+package com.pbw.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
